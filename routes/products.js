3e75ce0a5027f4838cfc34550fc4b4035dde5a1b
MFerreiroFr <mferreiro@iesjoandaustria.org>
2019-02-09 17:05:54 +0100

    Add create product

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,18 @@
 const Product = require('../models/Product');
 
-module.exports = app =>  {
-
-
-  app.get('/products', async(req, res) => {
+module.exports = app => {
+  app.get('/products', async (req, res) => {
     const products = await Product.find({});
     res.render('products/products', { products });
-  })
-}
+  });
+
+  app.get('/products/new', (req, res) => {
+    res.render('products/newProduct');
+  });
+
+  app.post('/products', async (req, res) => {
+    const newProduct = req.body;
+    await Product.create(newProduct);
+    res.redirect('/products');
+  });
+};

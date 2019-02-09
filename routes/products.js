11090015031a284b5e61daf1b9976ecb5873712c
MFerreiroFr <mferreiro@iesjoandaustria.org>
2019-02-09 17:49:40 +0100

    Add details, update and delete routes for products

--- routes/products.js
+++ routes/products.js
@@ -15,4 +15,25 @@ module.exports = app => {
     await Product.create(newProduct);
     res.redirect('/products');
   });
+
+  app.get('/products/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.render('products/detail', { product });
+  });
+
+  app.get('/products/edit/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.render('products/edit', { product });
+  });
+
+  app.post('/products/edit/:id', async(req,res) => {
+    const updatedProduct = req.body;
+    await Product.findByIdAndUpdate(req.params.id, updatedProduct);
+    res.redirect('/products');
+  });
+
+  app.post('/products/delete/:id', async(req,res) => {
+    await Product.findByIdAndDelete(req.params.id);
+    res.redirect('/products');
+  })
 };

a32900438844dfd970086a4a609cc9e1d234065f
MFerreiroFr <mferreiro@iesjoandaustria.org>
2019-02-11 12:03:43 +0100

    add filter to products get route to show non-deleted products

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,7 @@ const Product = require('../models/Product');
 
 module.exports = app => {
   app.get('/products', async (req, res) => {
-    const products = await Product.find({});
+    const products = await Product.find({deleted: false});
     res.render('products/products', { products });
   });
 

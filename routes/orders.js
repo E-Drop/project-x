51e5624ecf12e1f7f7a94b141ceaf767fa6a8c2a
MFerreiroFr <mferreiro@iesjoandaustria.org>
2019-02-12 15:29:58 +0100

    create orders

--- routes/orders.js
+++ routes/orders.js
@@ -17,21 +17,28 @@ module.exports = app => {
     const { products } = req.body;
     let i = 0;
     for (let product of products) {
-      await Product.findOneAndUpdate(
-        { name: product.name },
-        { $inc: { stock: (product.quantity * (-1)) } }
-      );
-      const price = await Product.findOne(
-        { name: product.name },
-        { _id: 0, price: 1 }
-      );
-      products[i] = { ...product, ...price };
-      i++;
+      try {
+        await Product.findOneAndUpdate(
+          { name: product.name },
+          { $inc: { stock: (product.quantity * (-1)) } }
+        );
+        const  { price }= await Product.findOne(
+          { name: product.name },
+          { _id: 0, price: 1 }
+        );
+        
+        const { name, quantity } = product;
+        products[i] = { name, quantity, price };
+        i++;
+      } catch(error) {
+        next(error)
+      }
     }
-  
-  await Order.insertOne({products: products, _store: req.session.currentUser._store})
-
-  res.redirect('/orders');
-
+    try {
+      await Order.create({products: products, _store: req.session.currentUser._store})
+    } catch(error) {
+      next(error)
+    }
+  res.send("Order created successfully");
   });
 };
